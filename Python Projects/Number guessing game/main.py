5d092304c953ef5e65aaba2e9df3907880465c4c
first <wwe.prasunsedaieng@gmail.com>
2024-09-20 13:11:22 +0545

    i

--- Python Projects/Number guessing game/main.py
+++ Python Projects/Number guessing game/main.py
@@ -14,14 +14,15 @@ def easy():
     print("Now you have 5 chance use it wisely. Good Luck!!!\n")
     print("The correct number is between (1-50) ")
     guesses = 5
-    number = random.randint(1, 50) 
+    number = random.randint(1, 50)
     while guesses > 0:
+
         try:
 
             guess = int(input("Enter your guess: "))
             if guess == number:
                 print("\nCongralutation, You have won!! ")
-                playagain()
+                playagain(easy)
                 break
             elif guess > 50 or guess < 1:
                 print("\nYou have to guess within 1 to 50")
@@ -37,24 +38,64 @@ def easy():
 
             if guesses == 0 and guess !=number:
                 print("Game Over, You have lost!! The correct answer was " + str(number))
-                playagain()
+                playagain(easy)
             
         
         except ValueError:
             print("\nPlease enter number, not string")
      
-     
 
-def playagain():
+def medium():
+    print("———————"*5)
+    print("\nGreat, You have decided to go with hard level")
+    print("Now you have 3 chance use it wisely. Good Luck!!!\n")
+    print("The correct number is between (1-50) ")
+    gusess = 3
+    number = random.randint(1,50)
+    while gusess > 0:
+
+        try:
+            
+            guess = int(input("Enter your guess: \n"))
+            if guess == number:
+                print("Congralutation you have won!\n")
+                playagain(medium)
+                break
+            elif guess > 50 or guess < 1:
+                print("You have to guess between 1 to 50 ")
+            elif guess > number:
+                print("You guessed too high, try getting lower\n")
+                guesses -= 1
+                print(f"You have {gusess} left now")
+            elif guess < number:
+                print("You guessed too low, try getting higher\n")
+                guesses -= 1
+                print(f"You have {gusess} left now")
+
+            if guess == 0 and guess != number:
+                print("Game Over, You have lost!! The correct answer was " + str(number))
+                playagain(medium)
+
+
+        except ValueError:
+            print("\nPlease enter number, not string")
+        
+
+
+
+
+
+def playagain(level_function):
     print("\nPress 1 to try again \nOr press 2 to exit the game!")
     again = input("What would you like to do: ")
     if again == "1":
-        easy()
+        level_function()
     elif again == "2":
         print("Thanks for playing the game!!")
+        gamemenu()
     else:
         print("Please enter the correct number!")
-        playagain() 
+        playagain(level_function)
 
 
 
@@ -70,6 +111,9 @@ def gamemenu():
         choice = int(input("Enter your choice: "))
         if choice == 1:
             easy()
+        elif choice == 2:
+            medium()
+
         else:
             print("PLEASE ENTER CORRECT NUMBER")
             gamemenu()
@@ -77,9 +121,4 @@ def gamemenu():
         print("PLEASE ENTER A NUMBER NOT STRINGS\n")
         gamemenu()
 
-
-
 gamemenu()
-
-
-
